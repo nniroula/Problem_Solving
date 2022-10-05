f5f0d5daa7c63b80a650bd0a544627694bd1757c
nabin <nkbhaie2017@gmail.com>
2022-10-05 16:15:05 -0600

    add the logic for converting two arrays into an object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,30 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+    let newObject = {};
+
+    if(arr2.length > arr1.length){
+        arr2.splice(arr1.length);
+    }
+
+    for(let i = 0; i < arr1.length; i++){
+        newObject[arr1[i]] = arr2[i] || null;
+    }
+
+    return newObject;
 }
+
+
+/*
+twoArrayObject
+Write a function called twoArrayObject which accepts two arrays of varying lengths.The first array consists of 
+keys and the second one consists of values. Your function should return an object created from the keys and values.
+ If there are not enough values, the rest of keys should have a value of null. If there not enough keys, just 
+ ignore the rest of values.
+
+Examples:
+
+twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3]) // {'a': 1, 'b': 2, 'c': 3, 'd': null}
+twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4]) // {'a': 1, 'b': 2, 'c': 3}
+twoArrayObject(['x', 'y', 'z'], [1, 2]) // {'x': 1, 'y': 2, 'z': null}
+
+*/
