2942614d4d86962a0dc5afcdf5eee954a65db395
nabin <nkbhaie2017@gmail.com>
2022-10-05 15:47:56 -0600

    add logic to find the average in average pair function

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, targetAverage) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+
+    while(leftIndex < rightIndex){
+        let averageOfTwoValues = (arr[leftIndex] + arr[rightIndex])/2;
+        if(averageOfTwoValues === targetAverage) return true;
+
+        if(averageOfTwoValues > targetAverage){
+            rightIndex -= 1;
+        }
+        
+        if(averageOfTwoValues < targetAverage){
+            leftIndex += 1;
+        }
+       
+    }
+    return false;
 }
