2db0ee24ef49bf011f7f242a13a1826abec37c26
nabin <nkbhaie2017@gmail.com>
2022-10-05 15:06:17 -0600

    add a logic for the construct note function

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+    if(message.length > letters.length) return false;
+
+    let chars = letters.split('');
+
+    for(let i = 0; i < message.length; i++){
+        let index = chars.indexOf(message[i]);
+
+        if(index !== -1){
+            chars.splice(index, 1);
+        }else{
+            return false;
+        }
+    }
+    return true;
 }
